144e4727f2b1793d298647ef264fa321a13d20f1
Cam <camlatimer@gmail.com>
2016-04-04 16:27:48 -0400

    got array of countries to show in the view

--- snackApp/js/snacks/index.controller.js
+++ snackApp/js/snacks/index.controller.js
@@ -9,6 +9,21 @@
         ]);
 
     function IndexControllerFunction(SnackFactory) {
-      this.snacks = SnackFactory.query();
-    }
+      var vm = this;
+      vm.snacks = SnackFactory.query();
+      vm.countriesFound = [];
+      vm.countries = SnackFactory.query(function(response){
+        response.forEach(function(snack){
+            for(var prop in snack){
+              if(prop == 'country'){
+                var countryCheck = vm.countriesFound.indexOf(snack[prop]);
+                if(countryCheck == -1){
+                  vm.countriesFound.push(snack[prop]);
+                }
+              }
+            }
+        });
+      });
+        console.log(vm.countriesFound);
+    };
 })();
