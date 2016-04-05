60a07fb02048fef18d3190b702ab31d3cadf1466
Toni Langley <chibia2@gmail.com>
2016-04-05 06:14:44 -0400

    trying to add functionality for comments

--- snackApp/js/snacks/show.controller.js
+++ snackApp/js/snacks/show.controller.js
@@ -5,10 +5,32 @@
   .module('snacks')
   .controller('ShowController', [
     "SnackFactory",
+    "CommentFactory",
     "$stateParams",
     ShowFunction
-  ])
-  function ShowFunction(SnackFactory, $stateParams){
+  ]);
+
+  function ShowFunction(SnackFactory, CommentFactory, $stateParams){
+    var vm = this;
     this.snack = SnackFactory.get({id: $stateParams.id});
+    // this.comment = CommentFactory.get({snack_id: $stateParams.snackId});
+
+//doesn't work yet
+    CommentFactory.query({snack_id: $stateParams.snack_id});
+
+    vm.comment = new CommentFactory();
+    vm.comment.snack_id = 32;
+    this.create = function(){
+      // this.comment.$save();
+      CommentFactory.save(vm.comment, function(test){
+        console.log(response)
+      });
+    };
+
+
+    // this.comment = new CommentFactory();
+    // this.create = function(){
+    //   this.comment.$save();
+    // };
   }
 })();
